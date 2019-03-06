a8734d393a7ec7ddb33f0edc5da0f616233813ba
Paul Myburgh <paulmyburgh88@gmail.com>
2019-03-06 18:58:49 +0100

    Made update listeners for store state mount instantly before render

--- src/useStoreState.ts
+++ src/useStoreState.ts
@@ -2,8 +2,15 @@ const shallowEqual = require("fbjs/lib/shallowEqual");
 
 // S = State
 // SS = Sub-state
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Store } from "./Store";
+
+interface IUpdateRef {
+  shouldUpdate: boolean;
+  onStoreUpdate: () => void;
+  getSubState: any;
+  currentSubState: any;
+}
 
 function useStoreState<S = any>(store: Store<S>): S;
 function useStoreState<S = any, SS = any>(store: Store<S>, getSubState: (state: S) => SS): SS;
@@ -11,23 +18,33 @@ function useStoreState(store: Store, getSubState?: (state) => any): any {
   const [subState, setSubState] = useState<any>(() =>
     getSubState ? getSubState(store.getRawState()) : store.getRawState()
   );
-  let shouldUpdate = true;
 
-  function onStoreUpdate() {
-    const nextSubState = getSubState ? getSubState(store.getRawState()) : store.getRawState();
-    if (shouldUpdate && !shallowEqual(subState, nextSubState)) {
+  const updateRef = useRef<IUpdateRef>({
+    shouldUpdate: true,
+    onStoreUpdate: null,
+    getSubState,
+    currentSubState: null,
+  });
+
+  updateRef.current.currentSubState = subState;
+  updateRef.current.getSubState = getSubState;
+
+  const onStoreUpdate = useCallback(() => {
+    const nextSubState = updateRef.current.getSubState ? updateRef.current.getSubState(store.getRawState()) : store.getRawState();
+    if (updateRef.current.shouldUpdate && !shallowEqual(updateRef.current.currentSubState, nextSubState)) {
       setSubState(nextSubState);
     }
+  }, []);
+
+  if (updateRef.current.onStoreUpdate === null) {
+    updateRef.current.onStoreUpdate = onStoreUpdate;
+    store._addUpdateListener(updateRef.current.onStoreUpdate);
   }
 
-  useEffect(() => {
-    store._addUpdateListener(onStoreUpdate);
-
-    return () => {
-      shouldUpdate = false;
+  useEffect(() => () => {
+      updateRef.current.shouldUpdate = false;
       store._removeUpdateListener(onStoreUpdate);
-    };
-  }, [getSubState]);
+  }, []);
 
   return subState;
 }
