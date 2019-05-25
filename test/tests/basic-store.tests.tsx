517c70d4e5ed84198bb9d64dce886c24d0146c56
Paul Myburgh <paulmyburgh88@gmail.com>
2019-05-25 14:40:22 +0200

    [test] added test for subscription, watched and prevWatched checks

---
+++ test/tests/basic-store.tests.tsx
@@ -0,0 +1,40 @@
+import { Store } from "../../src";
+
+interface ITestStore {
+  eggs: string[];
+  touched: boolean;
+}
+
+function getNewStore(): Store<ITestStore> {
+  return new Store({
+    eggs: ["green"],
+    touched: false,
+  });
+}
+
+describe("Store operations", () => {
+  it("should be able to subscribe to changes", () => {
+    const store = getNewStore();
+
+    store.subscribe(s => s.touched, (watched) => {
+      expect(watched).toEqual(true);
+    });
+
+    store.update(s => {
+      s.touched = true;
+    });
+  });
+
+  it("Should give the previous value when subscription gets a new value", () => {
+    const store = getNewStore();
+
+    store.subscribe(s => s.touched, (watched, all, prevWatched) => {
+      expect(watched).toEqual(true);
+      expect(prevWatched).toEqual(false);
+    });
+
+    store.update(s => {
+      s.touched = true;
+    });
+  });
+})
