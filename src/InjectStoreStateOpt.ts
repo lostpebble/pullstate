92dc38fd0f486a57e65fcb4db57654fd9f2663c4
Paul Myburgh <paulmyburgh88@gmail.com>
2019-07-24 17:55:20 +0200

    [InjectStoreStateOpt] managed to get types working on children React function for passed props

--- src/InjectStoreStateOpt.ts
+++ src/InjectStoreStateOpt.ts
@@ -1,19 +1,59 @@
 import React from "react";
 import { Store } from "./Store";
 import { useStoreStateOpt } from "./useStoreStateOpt";
-import { DeepKeyOfArray, TAllPathsParameter } from "./useStoreStateOpt-types";
+import { DeepTypeOfArray, TAllPathsParameter } from "./useStoreStateOpt-types";
 
-export interface IPropsInjectStoreStateOpt<S = any, P extends TAllPathsParameter<S> = TAllPathsParameter<S>> {
+export interface IPropsInjectStoreStateOpt<
+  S = any,
+  P extends TAllPathsParameter<S> = TAllPathsParameter<S>,
+  O extends [
+    DeepTypeOfArray<S, P[0]>,
+    DeepTypeOfArray<S, P[1]>,
+    DeepTypeOfArray<S, P[2]>,
+    DeepTypeOfArray<S, P[3]>,
+    DeepTypeOfArray<S, P[4]>,
+    DeepTypeOfArray<S, P[5]>,
+    DeepTypeOfArray<S, P[6]>,
+    DeepTypeOfArray<S, P[7]>,
+    DeepTypeOfArray<S, P[8]>,
+    DeepTypeOfArray<S, P[9]>,
+    DeepTypeOfArray<S, P[10]>
+  ] = [
+    DeepTypeOfArray<S, P[0]>,
+    DeepTypeOfArray<S, P[1]>,
+    DeepTypeOfArray<S, P[2]>,
+    DeepTypeOfArray<S, P[3]>,
+    DeepTypeOfArray<S, P[4]>,
+    DeepTypeOfArray<S, P[5]>,
+    DeepTypeOfArray<S, P[6]>,
+    DeepTypeOfArray<S, P[7]>,
+    DeepTypeOfArray<S, P[8]>,
+    DeepTypeOfArray<S, P[9]>,
+    DeepTypeOfArray<S, P[10]>
+  ]
+> {
   store: Store<S>;
   paths: P;
-  children: (output: any[]) => React.ReactElement;
+  children: (output: O) => React.ReactElement;
 }
 
-export function InjectStoreStateOpt<S = any>({
-  store,
-  paths,
-  children,
-}: IPropsInjectStoreStateOpt<S>): React.ReactElement {
-  const state = useStoreStateOpt(store, paths);
+export function InjectStoreStateOpt<
+  S extends object,
+  P extends TAllPathsParameter<S>,
+  O extends [
+    DeepTypeOfArray<S, P[0]>,
+    DeepTypeOfArray<S, P[1]>,
+    DeepTypeOfArray<S, P[2]>,
+    DeepTypeOfArray<S, P[3]>,
+    DeepTypeOfArray<S, P[4]>,
+    DeepTypeOfArray<S, P[5]>,
+    DeepTypeOfArray<S, P[6]>,
+    DeepTypeOfArray<S, P[7]>,
+    DeepTypeOfArray<S, P[8]>,
+    DeepTypeOfArray<S, P[9]>,
+    DeepTypeOfArray<S, P[10]>
+  ]
+>({ store, paths, children }: IPropsInjectStoreStateOpt<S, P, O>): React.ReactElement {
+  const state = useStoreStateOpt(store, paths) as O;
   return children(state);
 }
