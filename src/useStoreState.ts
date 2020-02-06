ac45648e091dacaa7359ce8005a1d4c1ce083fd2
Paul Myburgh <paulmyburgh88@gmail.com>
2020-02-06 11:00:22 +0100

    bugfix for making sure deps in useStoreState works correctly

--- src/useStoreState.ts
+++ src/useStoreState.ts
@@ -8,44 +8,58 @@ import { Store } from "./Store";
 export interface IUpdateRef {
   shouldUpdate: boolean;
   onStoreUpdate: (() => void) | null;
-  getSubState: any;
+  getSubState?: (state) => any;
   currentSubState: any;
+  setInitial: boolean;
 }
 
 function useStoreState<S = any>(store: Store<S>): S;
-function useStoreState<S = any, SS = any>(store: Store<S>, getSubState: (state: S) => SS, deps?: ReadonlyArray<any>): SS;
+function useStoreState<S = any, SS = any>(
+  store: Store<S>,
+  getSubState: (state: S) => SS,
+  deps?: ReadonlyArray<any>
+): SS;
 function useStoreState(store: Store, getSubState?: (state) => any, deps?: ReadonlyArray<any>): any {
-  const [subState, setSubState] = useState<any>(() =>
-    getSubState ? getSubState(store.getRawState()) : store.getRawState()
-  );
-
   const updateRef = useRef<IUpdateRef>({
     shouldUpdate: true,
     onStoreUpdate: null,
     getSubState,
     currentSubState: null,
+    setInitial: false,
   });
 
-  updateRef.current.currentSubState = subState;
-  updateRef.current.getSubState = getSubState;
+  const [, setUpdateTrigger] = useState(0);
+
+  if (!updateRef.current.setInitial) {
+    updateRef.current.currentSubState = updateRef.current.getSubState
+      ? updateRef.current.getSubState(store.getRawState())
+      : store.getRawState();
+    updateRef.current.setInitial = true;
+  }
 
   if (updateRef.current.onStoreUpdate === null) {
     updateRef.current.onStoreUpdate = function onStoreUpdate() {
-      const nextSubState = updateRef.current.getSubState ? updateRef.current.getSubState(store.getRawState()) : store.getRawState();
+      const nextSubState = updateRef.current.getSubState
+        ? updateRef.current.getSubState(store.getRawState())
+        : store.getRawState();
       if (updateRef.current.shouldUpdate && !isEqual(updateRef.current.currentSubState, nextSubState)) {
         // final check again before actually running state update (might prevent no-op errors with React)
         if (updateRef.current.shouldUpdate) {
-          setSubState(nextSubState);
+          updateRef.current.currentSubState = nextSubState;
+          setUpdateTrigger(val => val + 1);
         }
       }
     };
     store._addUpdateListener(updateRef.current.onStoreUpdate);
   }
 
-  useEffect(() => () => {
+  useEffect(
+    () => () => {
       updateRef.current.shouldUpdate = false;
       store._removeUpdateListener(updateRef.current.onStoreUpdate!);
-  }, []);
+    },
+    []
+  );
 
   if (deps !== undefined) {
     const prevDeps = useRef<ReadonlyArray<any>>(deps);
