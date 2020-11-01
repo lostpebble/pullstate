f0b12403d5edc6647634c35caa4c8dd8f2dda83f
David Fava <dfava80@gmail.com>
2020-11-01 11:26:15 +0000

    Add support for Redux DevTools on RND

--- src/reduxDevtools.ts
+++ src/reduxDevtools.ts
@@ -5,16 +5,16 @@ interface IORegisterInDevtoolsOptions {
 }
 
 export function registerInDevtools(stores: IPullstateAllStores, { namespace = "" }: IORegisterInDevtoolsOptions = {}) {
-  if (typeof document !== "undefined") {
+  const devToolsExtension = (window as any)?.__REDUX_DEVTOOLS_EXTENSION__;
+
+  if (devToolsExtension) {
     for (const key of Object.keys(stores)) {
       const store = stores[key];
 
-      const devToolsExtension = (window as any).__REDUX_DEVTOOLS_EXTENSION__;
-      if (devToolsExtension) {
-        const devTools = devToolsExtension.connect({ name: `${namespace}${key}` });
-        devTools.init(store.getRawState());
-        let ignoreNext = false;
-        /*store.subscribe(
+      const devTools = devToolsExtension.connect({ name: `${namespace}${key}` });
+      devTools.init(store.getRawState());
+      let ignoreNext = false;
+      /*store.subscribe(
           (state) => {
             if (ignoreNext) {
               ignoreNext = false;
@@ -24,25 +24,24 @@ export function registerInDevtools(stores: IPullstateAllStores, { namespace = ""
           },
           () => {}
         );*/
-        store.subscribe(
-          (s) => s,
-          (watched) => {
-            if (ignoreNext) {
-              ignoreNext = false;
-              return;
-            }
-            devTools.send("Change", watched);
+      store.subscribe(
+        (s) => s,
+        (watched) => {
+          if (ignoreNext) {
+            ignoreNext = false;
+            return;
           }
-        );
+          devTools.send("Change", watched);
+        }
+      );
 
-        devTools.subscribe((message: { type: string; state: any }) => {
-          if (message.type === "DISPATCH" && message.state) {
-            ignoreNext = true;
-            const parsed = JSON.parse(message.state);
-            store.replace(parsed);
-          }
-        });
-      }
+      devTools.subscribe((message: { type: string; state: any }) => {
+        if (message.type === "DISPATCH" && message.state) {
+          ignoreNext = true;
+          const parsed = JSON.parse(message.state);
+          store.replace(parsed);
+        }
+      });
     }
   }
 }
