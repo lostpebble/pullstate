976d5a49f070be2ef0b62abfeecd122a1fe89561
Paul Myburgh <lostpebble@protonmail.com>
2025-08-17 11:10:20 +0200

    feat: publish with better react

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,8 +1,5 @@
 export default {
   nodeProtocol: false,
   target: "chrome100",
-  platform: "neutral",
-  inputOptions: {
-    jsx: 'react', // Use classic JSX transformation
-  },
+  platform: "neutral"
 };
